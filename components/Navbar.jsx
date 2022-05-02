a30c5ecbb345b8f096d2d561856444a1c4209f69
Amrindra <amrindraouk@yahoo.com>
2022-05-02 18:40:36 -0400

    1 file modified, 1 added

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,11 @@
+import styles from '../styles/Navbar.module.css';
+import { FaFacebook, FaPhoneSquare } from 'react-icons/fa';
+import { SiInstagram } from 'react-icons/si';
+
 const Navbar = () => {
   return (
     <div>
-      <header className="navbar-container">
+      <header className={styles.navbar_container}>
         <div className="navbar-logo">
           <h1>Crystal Nails</h1>
         </div>
@@ -13,9 +17,10 @@ const Navbar = () => {
           <li className="nav-list-item">Appointment</li>
           <li className="nav-list-item">ABout us</li>
         </ul>
-
         <div className="navbar-icons">
-        <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
+          <SiInstagram />
+          <FaFacebook />
+          <FaPhoneSquare />
         </div>
       </header>
     </div>
