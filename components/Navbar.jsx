b0ba1bf70d9a77341605369f46c795ca16cb504d
Amrindra <amrindraouk@yahoo.com>
2022-05-02 17:59:03 -0400

    3 files modified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -5,13 +5,18 @@ const Navbar = () => {
         <div className="navbar-logo">
           <h1>Crystal Nails</h1>
         </div>
-        <ul className="navbar-">
+
+        <ul className="navbar-link-items">
           <li className="nav-list-item">Home</li>
           <li className="nav-list-item">Service</li>
           <li className="nav-list-item">Contact us</li>
           <li className="nav-list-item">Appointment</li>
           <li className="nav-list-item">ABout us</li>
         </ul>
+
+        <div className="navbar-icons">
+        <FontAwesomeIcon icon="fa-brands fa-facebook-f" />
+        </div>
       </header>
     </div>
   );
