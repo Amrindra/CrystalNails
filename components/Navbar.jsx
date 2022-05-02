2248811357726c47e5a9b854e6d0e808a490df2a
Amrindra <amrindraouk@yahoo.com>
2022-05-02 15:45:54 -0400

    2 files modified

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,4 +1,19 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <header className="navbar-container">
+        <div className="navbar-logo">
+          <h1>Crystal Nails</h1>
+        </div>
+        <ul className="navbar-">
+          <li className="nav-list-item">Home</li>
+          <li className="nav-list-item">Service</li>
+          <li className="nav-list-item">Contact us</li>
+          <li className="nav-list-item">Appointment</li>
+          <li className="nav-list-item">ABout us</li>
+        </ul>
+      </header>
+    </div>
+  );
 };
 export default Navbar;
