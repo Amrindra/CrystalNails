0ddfddff37342846d8aff5b73a27e3e76ebd0a62
Amrindra <amrindraouk@yahoo.com>
2022-05-03 19:47:48 -0400

    2 files modified

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -12,7 +12,7 @@ const Banner = () => {
         /> */}
         <img
           className={styles.banner_image}
-          src="https://user-images.githubusercontent.com/70451928/166390465-fc137271-f827-4681-a6b4-85394b6936b5.jpg"
+          src="https://user-images.githubusercontent.com/70451928/166597375-6c9c15c3-0947-4986-93a1-c7b7744184ce.jpg"
           alt=""
         />
       </div>
