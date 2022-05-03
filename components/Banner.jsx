8479402efd0638d91ecf7aa8422091edcd7da4d5
Amrindra <amrindraouk@yahoo.com>
2022-05-03 17:40:27 -0400

    1 binaries uploaded

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -5,27 +5,27 @@ const Banner = () => {
   return (
     <div className={styles.banner_container}>
       <div className={styles.banner_image}>
-        {/* <Image
+        <Image
           src="https://user-images.githubusercontent.com/70451928/166390465-fc137271-f827-4681-a6b4-85394b6936b5.jpg"
           layout="fill"
           objectFit="cover"
-        /> */}
-
-        <img
-          className={styles.banner_image}
-          src="https://user-images.githubusercontent.com/70451928/166390465-fc137271-f827-4681-a6b4-85394b6936b5.jpg"
-          alt=""
         />
+      </div>
+      {/* 
+      <img
+        className={styles.banner_image}
+        src="https://user-images.githubusercontent.com/70451928/166390465-fc137271-f827-4681-a6b4-85394b6936b5.jpg"
+        alt=""
+      /> */}
 
-        <div className={styles.banner_intro}>
+      {/* <div className={styles.banner_intro}>
           <h1 className={styles.banner_title}>
             Welcome <br />
             <span className={styles.banner_sub_title}>
               To a Full Beauty & CO Experience
             </span>
           </h1>
-        </div>
-      </div>
+        </div> */}
     </div>
   );
 };
