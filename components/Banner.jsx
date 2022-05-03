5884239dae2f2172dccb3b5b6eafd041615a4130
Amrindra <amrindraouk@yahoo.com>
2022-05-03 19:45:24 -0400

    2 files modified

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Banner.module.css';
 const Banner = () => {
   return (
     <div className={styles.banner_container}>
-      <div className={styles.banner_image}>
+      <div>
         {/* <Image
           src="https://user-images.githubusercontent.com/70451928/166390465-fc137271-f827-4681-a6b4-85394b6936b5.jpg"
           layout="fill"
