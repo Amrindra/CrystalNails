3b7a098b781acae23b1b212edd904168e4c62785
Amrindra <amrindraouk@yahoo.com>
2022-05-03 18:56:48 -0400

    5 files modified, 1 deleted

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Banner from '../components/Banner';
 import Navbar from '../components/Navbar';
 import Highlight from '../components/Highlight';
+import Image from 'next/image';
 
 export default function Home() {
   return (
